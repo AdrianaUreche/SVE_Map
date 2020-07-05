0ac6facbc216d080f63ba4a9e7ac6cbe02355cef
Darren Bleuel <darren@nukees.com>
2020-07-05 11:20:09 -0800

    Breaking out three header php includes

--- SVE Map Code/generic.php
+++ SVE Map Code/generic.php
@@ -1,6 +1,8 @@
-<?php include("menubar.php");?>
+<?php include("db.php");?>  <!-- Login Session and database functions -->
+<?php include("header.php");?>  <!-- Header. Replace if you want to customize -->
+<?php include("menubar.php");?>  <!-- Common top menu bar -->
 
 <!--CONTENT GOES HERE-->
 
-<?php include("tail.php");?>
+<?php include("tail.php");?>  <!-- Contact inf and end body/html tags->
 
