8e02318acd1ce0acb1f6e908243aef0cacfdcdc1
Darren Bleuel <darren@nukees.com>
2020-07-06 23:28:06 -0800

    Added Achievements function

--- SVE Map Code/db.php
+++ SVE Map Code/db.php
@@ -11,17 +11,44 @@ $teamname = htmlspecialchars($_SESSION["teamname"]);
 // Include config file
 require_once "config.php";
 
-$sql = "SELECT teamid, player_names FROM teams WHERE teamname = '$teamname'";
+$sql = "SELECT teamid, player_names, email, impact_factor, action_points, action_points_used FROM teams WHERE teamname = '$teamname'";
 
 if($result = mysqli_query($link, $sql)){
 	if(mysqli_num_rows($result) > 0){
 		$row = mysqli_fetch_array($result);
-		$teamid = $row['teamid'];
-		$player_names = $row['player_names'];
+		list($teamid, $player_names, $email, $impact, $action_points, $action_points_used) = $row;
 	}
 	// Free result set
 	mysqli_free_result($result);
 }
+
+function getachievements($link) {
+
+        $sql = "SELECT achid, name, description, flufftext, impact, max_num FROM achievements";
+
+        if($result = mysqli_query($link, $sql)){
+                if(mysqli_num_rows($result) > 0){
+                        while($row = mysqli_fetch_array($result)){
+				$achid = $row['achid'];
+				list($achid, $achname[$achid], $achdescription[$achid], $achfluff[$achid], $achimpact[$achid], $achmaxnum[$achid]) = $row;
+                                $nsql = "SELECT teams.teamname, teams.teamid from achieved, teams where achieved.achid = '$achid' AND achieved.teamid = teams.teamid";
+				if($achresult = mysqli_query($link, $nsql)){
+		                        while($arow = mysqli_fetch_array($achresult)){
+						$ateam = $arow['teamname'];
+						list($achteam[$achid][$ateam], $achflag[$achid][$ateam]) = $arow;
+					}
+
+				}
+				mysqli_free_result($achresult);
+
+                        }
+                }
+                // Free result set
+                mysqli_free_result($result);
+        }
+        return array($achname, $achdescription, $achfluff, $achimpact, $achmaxnum, $achteam, $achflag);
+}
+
 
 function getblocks($link) {
 
