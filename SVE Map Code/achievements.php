6a7fc4791e285331d467a5a0983da93a7660d255
Darren Bleuel <darren@nukees.com>
2020-07-13 20:56:13 -0800

    Blocks can now be owned

--- SVE Map Code/achievements.php
+++ SVE Map Code/achievements.php
@@ -4,21 +4,24 @@
 
 <?php list($achname, $achdescription, $achfluff, $achimpact, $achmaxnum, $achteam, $achflag)=getachievements($link); 
 
-
-echo "<ul>";
-foreach($achname as $aid => $aname) {
-	echo "<li>",$aname,"</li>\n";
-	echo "<ol><li>",$achfluff[$aid],"</li>\n";
-	echo "<li>",$achdescription[$aid],"</li>\n";
-	echo "<li> Impact = ",$achimpact[$aid],"</li>\n";
-	echo "<li> Max number = ",$achmaxnum[$aid],"</li>\n<ol>\n";
-	foreach($achteam[$aid] as $atid => $ateam) {
-		echo "<li> ",$ateam,"</li>\n";
-		echo "<li> Team number = ",$achflag[$aid][$atid],"</li>\n";
+if(isset($achname)) {
+	echo "<ul>";
+	foreach($achname as $aid => $aname) {
+		echo "<li>",$aname,"</li>\n";
+		echo "<ol><li>",$achfluff[$aid],"</li>\n";
+		echo "<li>",$achdescription[$aid],"</li>\n";
+		echo "<li> Impact = ",$achimpact[$aid],"</li>\n";
+		echo "<li> Max number = ",$achmaxnum[$aid],"</li>\n<ol>\n";
+		if(isset($achteam[$aid])) {
+			foreach($achteam[$aid] as $atid => $ateam) {
+				echo "<li> ",$ateam,"</li>\n";
+				echo "<li> Team number = ",$achflag[$aid][$atid],"</li>\n";
+			}
+		}
+		echo "</ol></ol>";
 	}
-	echo "</ol></ol>";
+	echo "</ul>";
 }
-echo "</ul>";
 
 ?>
 
