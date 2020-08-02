6014e3b89feeaa393fd0982d7681f6ad9dc083c3
Darren Bleuel <darren@nukees.com>
2020-08-01 16:08:06 -0800

    ALL ABILITIES WORKING (in theory)!

--- SVE Map Code/ability.php
+++ SVE Map Code/ability.php
@@ -96,8 +96,8 @@ function occupyblocks($link, $teamid, $newblock) {
 			error("Error updating record: " . mysqli_error($link));
 		}
 	}
+	return TRUE;
 }
- 
 
 function flipblocks($link, $teamid, $newblock) {
 
@@ -105,6 +105,7 @@ function flipblocks($link, $teamid, $newblock) {
 	$sqltworoot = "SELECT side,teamid FROM blocksides WHERE blockid = ";
 
         foreach($newblock as $block) {
+	
                 $sql = $sqloneroot.$block;
 //		echo $sql,"<br>";
 		if($result = mysqli_query($link, $sql)) {
@@ -113,6 +114,8 @@ function flipblocks($link, $teamid, $newblock) {
 			}
 		}
 		mysqli_free_result($result);
+
+		$blockside = Array();
 
 		$sql = $sqltworoot.$block;
 //		echo $sql,"<br>";
@@ -126,16 +129,18 @@ function flipblocks($link, $teamid, $newblock) {
                         }
                 }
 //		echo "SIDES: ",$sides;
-		for($i=$sides;$i>=0;$i--) {
+//	   	if($sides<2)error("Sorry, at least one of the blocks you selected (".$blockname[$block].") has one (or fewer) sides and can't be flipped.  Please consult a topologist.");
+
+		for($i=$sides-1;$i>=0;$i--) {
 //			echo $i,":",$blockside[$i],"->",$blockside[$i-1],"<br>";
 			$blockside[$i+1] = $blockside[$i];
 		}
-		$blockside[0]=$blockside[$sides+1];
+		$blockside[0]=$blockside[$sides];
 		if ($blockside[0]==0) $blockside[0]=$teamid;
 //		echo "BS:",$blockside[1];
 
 		foreach($blockside as $i => $team) {
-			if(isset($blockside[$i]) && $i<=$sides) {
+			if(isset($blockside[$i]) && $i<=$sides-1) {
 				$sql = "INSERT INTO blocksides (teamid, blockid, side) VALUES (".$team.", ".$block.", ".$i.")";
 
 				if (isset($blockside[$i+1])) $sql = "UPDATE blocksides SET teamid = ".$team." WHERE blockid = ".$block." AND side = ".$i;
@@ -148,6 +153,7 @@ function flipblocks($link, $teamid, $newblock) {
 		}
 
 	}
+	return TRUE;
 }
 
 
@@ -214,7 +220,7 @@ case 1:
 //			echo "Number of blocks converted = ".sizeof($newblock)." (Oldblock = ".$oldblock.")<br>";
 		}
 	}
-	while (sizeof($newblock)>$oldblock && sizeof($newblock)<sizeof($blocksel) && $j++<10);
+	while (sizeof($newblock)>$oldblock && sizeof($newblock)<sizeof($blocksel) && $j++<=sizeof($blocksel));
 	if(sizeof($newblock)!=sizeof($blocksel))error("Sorry, you are not adjacent to the blocks you selected.");
 //	echo "NB: ".sizeof($newblock).", BS: ",sizeof($blocksel)."<br>\n";
 	occupyblocks($link, $teamid, $blocksel);
@@ -233,7 +239,7 @@ case 2:
 //                      echo "Number of blocks converted = ".sizeof($newblock)." (Oldblock = ".$oldblock.")<br>";
                 }
         }
-        while (sizeof($newblock)>$oldblock && sizeof($newblock)<sizeof($blocksel) && $j++<10);
+        while (sizeof($newblock)>$oldblock && sizeof($newblock)<sizeof($blocksel) && $j++<=sizeof($blocksel));
 	if(sizeof($newblock)!=sizeof($blocksel))error("Sorry, the blocks you selected must be adjacent to each other.");
 	
 //	echo "NBA: ".sizeof($newblock).", BS: ",sizeof($blocksel)."<br>\n";
@@ -273,31 +279,168 @@ case 2:
 		}
 	}
 	$impact -= ($minimum-1);
-	if($impact<sizeof($blocksel)) error("Sorry, you can only occupy as many blocks as your impact factor, minus the number of blocks you jumped.");
+
+	if($impact<sizeof($blocksel)) error("Sorry, you can only occupy as many blocks as your impact factor, minus the number of blocks you jumped (note that it takes three blocks to reach The Man from Esplanade).");
 
 //      echo "NB: ".sizeof($newblock).", BS: ",sizeof($blocksel)."<br>\n";
 	occupyblocks($link, $teamid, $blocksel);
 	
 	break;
 case 3:
-	error("Sorry, I haven't programmed this yet.  Why don't you try an unoccupied block?");
+	if(sizeof($blocksel)==0) error("You didn't select any blocks!");
+        if(0.5+$impact/2<sizeof($blocksel)) error("Sorry, you can only flip as many blocks as your impact factor divided by two (rounding up).");
+        do {
+                $oldblock=sizeof($newblock);
+                foreach($blocksel as $blockid) {
+//                        if($blockownid[$blockid]!=0) error("Sorry, you can only use the \\\"Occupy Adjacent Blocks\\\" ability on unowned playa blocks.");
+//                      echo "<br>".$blockid."  NEWBLOCK=",$newblock[$blockid]."<br>";
+                        if(isneighbor($link, $blockid, $teamid, array()) || $blockownid[$blockid]==$teamid)$newblock[$blockid]=$teamid;
+//                      echo "Number of blocks converted = ".sizeof($newblock)." (Oldblock = ".$oldblock.")<br>";
+                }
+        }
+        while (sizeof($newblock)>$oldblock && sizeof($newblock)<sizeof($blocksel) && $j++<=sizeof($blocksel));
+        if(sizeof($newblock)!=sizeof($blocksel))error("Sorry, you are not adjacent to the blocks you selected.");
+
+	foreach ($newblock as $blockid => $i) {
+//        echo "FLIP: ",$blockid," => ",$i,"<br>";
+
+                $sqlroot = "SELECT num_sides,flippable FROM blocks WHERE blockid = ";
+		if($blockownid[$blockid]==666)error("Sorry, at least one block you selected ".$blockname[$blockid].") has been destroyed. Radiation abounds. Bring marshmallows.");
+
+                $sql = $sqlroot.$blockid;
+//              echo $sql,"<br>";
+                if($result = mysqli_query($link, $sql)) {
+                        if(mysqli_num_rows($result)==1) {
+                                list($sides,$flippable) = mysqli_fetch_array($result);
+				if($sides<2)error("Sorry, at least one of the blocks you selected (".$blockname[$blockid].") has one (or fewer) sides and can't be flipped.  Please consult a topologist.");
+				if(!$flippable)error("Sorry, at least one block you selected (".$blockname[$blockid].") is asymmetrical and not traditionally flippable.  You might try the Plaza Transport ability.");
+//				echo "SIDE: ",$sides,"; ",$flippable."<br>";
+			} else {
+				error("ARGH, there are the wrong number of blocks with ID=".$blockid." in the database. There should be one. Please report this to an administrator.");
+			}
+                }
+                mysqli_free_result($result);
+	}
+	flipblocks($link, $teamid, array_keys($newblock));
+
 	break;
 case 4:
 	if ($blockownid[314]==0) {
 		occupyblocks($link, $teamid, Array(314));
-		echo "OCCUPY: ",$blockownid[314],"<br>\n";
+//		echo "OCCUPY: ",$blockownid[314],"<br>\n";
 	} else {
 		flipblocks($link, $teamid, Array(314));
 	}
 	break;
 case 5;
-	error("Sorry, I haven't programmed this yet.  Why don't you try an unoccupied block?");
+	$dist = [1,2,2,3,4,4,5];
+//echo "IMPACT:",$impact,"<br>";
+	if(sizeof($blocksel)==0) error("You didn't select any blocks!");
+	$flippable = 0;
+	$teamsector = array();
+	foreach($teamblocks as $blockcheck) {
+		$sql = "SELECT time,end_row FROM blocks WHERE flippable=0 AND blockid = ".$blockcheck;
+		if($result = mysqli_query($link, $sql)){
+			if(mysqli_num_rows($result) == 1){
+				$row = mysqli_fetch_array($result);
+				$teamsector[$blockcheck]=$dist[substr($row['time'],0,1)-3]+($row['end_row']<8)*0.5;
+//				echo "TS: ",$teamsector[$blockcheck],"<br>";
+			}
+		}
+	}
+
+	if(sizeof($teamsector)==0)error("You have to be adjacent to a plaza, plaza road, or Center Camp to use it as a transport");
+
+	foreach($blocksel as $blockid) {
+		$sql = "SELECT time,letter,begin_sec,begin_row,num_sides FROM blocks WHERE flippable=0 AND blockid = ".$blockid;
+		$min = 6;
+		if($result = mysqli_query($link, $sql)){
+                        if(mysqli_num_rows($result) == 1){
+				list($time, $letter, $sec, $row, $sel_sides) = mysqli_fetch_array($result);
+				if ($sel_sides<2) error("At least one block you selected (".$time."&".$letter.") cannot be flipped it has only one side or less.  Weird, right?  Probably, it's permified.");
+
+				$oppendsec = 31-$sec;
+				$oppsql = "SELECT num_sides,blockid FROM blocks WHERE end_sec = ".$oppendsec." AND begin_row = ".$row;                 
+				if($nresult = mysqli_query($link, $oppsql)){
+					if(mysqli_num_rows($nresult) == 1){
+						list($num_sides,$oppblock) = mysqli_fetch_array($nresult);
+						if (in_array($oppblock, $blocksel)) error("You shouldn't select both a block and its opposite with which to be switched.");
+						$oppblocks[$blockid]=$oppblock;
+					}
+				}
+				mysqli_free_result($nresult);
+
+				if ($num_sides<2) error("At least one block you selected (".$time."&".$letter.") cannot be flipped with the opposite side because the opposite block has only one side or less.  Weird, right?  Probably, it's permified or destroyed.");
+				foreach($teamsector as $s) {
+//					echo "S: ",$s,"<br>";
+					$d=abs($dist[substr($time,0,1)-3]-intval($s))+abs(($row<8)-($s-intval($s))*2);
+//					echo "BS:".$blockid."--Old min: ",$min,"; D: ",$d,"; DIST: ",$dist[substr($time,0,1)-3],"; EXT: ",($row<8),"  ",($s-intval($s))*2,"<br>";
+					if($min>$d)$min=$d;
+				}
+			} else {
+				error("At least one of the blocks you selected was not next to a plaza.");
+			}
+		}
+		mysqli_free_result($result);
+		$impact-=($min+1);
+//		echo "IMPACT: ",$impact,"<br>";
+	}
+//	echo "FINAL IMPACT: ".$impact."<br>";
+	if($impact<0) error("You need as much impact factor as the number of blocks to be transported (including the opposing blocks), minus one for each plaza between any of your plaza-adjacent blocks and each of those selected.");
+	flipblocks($link, $teamid, array_values($blocksel));
+	flipblocks($link, $teamid, array_values($oppblocks));
+	foreach($blocksel as $block) {
+		$sql = "UPDATE blocksides SET blockid = 999 WHERE blockid = ".$block;
+		if (!mysqli_query($link, $sql)) {
+			error("Error updating record: " . mysqli_error($link));
+		}
+		$sql = "UPDATE blocksides SET blockid = ".$block." WHERE blockid = ".$oppblocks[$block];
+		if (!mysqli_query($link, $sql)) {
+			error("Error updating record: " . mysqli_error($link));
+		}      
+		$sql = "UPDATE blocksides SET blockid = ".$oppblocks[$block]." WHERE blockid = 999";
+		if (!mysqli_query($link, $sql)) {
+			error("Error updating record: " . mysqli_error($link));
+		}
+        }
+	
         break;
 case 6:
-        error("Sorry, I haven't programmed this yet.  Why don't you try an unoccupied block?");
+        if(sizeof($blocksel)==0) error("You didn't select any blocks!");
+        if($impact-1<sizeof($blocksel)) error("Sorry, you can only permify as many blocks as your impact factor minus one.");
+	foreach($blocksel as $blockid) {
+		$sql = "UPDATE blocks SET num_sides = 0 WHERE blockid = ".$blockid;
+		if (!mysqli_query($link, $sql)) {
+			error("Error updating record: " . mysqli_error($link));
+		}
+	}
         break;
 case 7:
-	error("Sorry, I haven't programmed this yet.  Why don't you try an unoccupied block?");
+	if(sizeof($blocksel)==0) error("You didn't select a block!");
+        if(sizeof($blocksel)!=1) error("You can only destroy one block per turn.");
+
+	$sql = "SELECT value FROM blocks WHERE blockid = ".$blocksel[0];
+
+	if($result = mysqli_query($link, $sql)){
+		if(mysqli_num_rows($result) == 1){
+			$value=mysqli_fetch_array($result)['value'];
+                }
+	}
+	if($impact<$value) error("You must have an impact factor (".$impact.") greater than the value of the block (".$value.") you would like to destroy.");
+
+	$sql = "UPDATE blocks SET num_sides = 0 WHERE blockid = ".$blocksel[0];
+	if (!mysqli_query($link, $sql)) {
+		error("Error updating record: " . mysqli_error($link));
+	}
+	$sql = "UPDATE blocksides SET teamid = 666 WHERE side = 0 AND blockid = ".$blocksel[0];
+	if (!mysqli_query($link, $sql)) {
+                error("Error updating record: " . mysqli_error($link));
+        }
+        $sql = "DELETE FROM blocksides WHERE side > 0 AND blockid = ".$blocksel[0];
+	if (!mysqli_query($link, $sql)) {
+                error("Error updating record: " . mysqli_error($link));
+        }
+
 	break;
 case 8:
 	$sql = "UPDATE teams SET impact_factor = ".(2+$impact)." WHERE teamid = ".$teamid;
