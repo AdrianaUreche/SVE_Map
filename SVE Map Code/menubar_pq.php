a6226187d8e8f9da0cb0027548ee874a387f34f5
crewesite <eric@crewesite.com>
2020-08-09 08:35:45 -0700

    new changes from eric

--- SVE Map Code/menubar_pq.php
+++ SVE Map Code/menubar_pq.php
@@ -3,11 +3,11 @@
 		<li class="customStyle"><a href="instructions.php">Game Board Rules</a></li>
         <li class="customStyle"><a href="abilities.php">Abilities</a></li>
 		<li class="customStyle"><a href="achievements.php">Achievements</a></li>
-		<li class="customStyle"><a href="teams.php">Team Portal</a></li>
+		<li class="customStyle"><a href="teams.php">Team Portal  (<font color="red"><b>Submit Puzzle/Quest Answer HERE!</b></font>)</a></li>
 
 		<li style="float: right;"><a><?php if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {echo "<i>Action Points</i> (<i>Turns Remaining</i>): ", $action_points;} else {echo "<i>Action Points</i> (<i>Turns Remaining</i>): N/A";}?></a></li>		
-		<li style="float: right;"><a><?php if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {echo "<i>Impact Factor</i>: ", $impact;} else {echo "<i>Impact Factor</i>: N/A";}?></a></li>		
-		<li style="float: right;"><a><?php if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {echo "<i>Team</i>: ", $teamname;} else {echo "<i>Team</i>: N/A";}?></a></li>	
+		<li style="float: right;"><a><?php if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {echo "<i>Impact Factor</i>: ", $impact;} else {echo "<i>Impact Factor</i>: N/A";}?></a></li>	
+		<li style="float: right;"><a><?php if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {echo "<i>Team</i>: ", $teamname;} else {echo "<i>Team</i>: N/A";}?></a></li>
 	</ul>
 
 <!--		<li class="customStyle"><a href="register.php">Registration</a></li>
---
+++ SVE Map Code/puzzle_template_manual.php
@@ -0,0 +1,72 @@
+<?php include("db.php");?>  <!-- Login Session and database functions -->
+<?php include("header.php");?>  <!-- Header. Replace if you want to customize -->
+<?php include("menubar.php");?>  <!-- Common top menu bar -->
+
+
+<html>
+<style>
+body, html {
+  height: 100%;
+  margin: 0;
+}
+
+
+.transbox {
+  width: 1300px;
+  padding: 50px;
+  margin: 20px;
+  background-color: #ffffff;
+  border: 1px solid black;
+  opacity: 1.0;
+}
+
+.transbox p {
+  margin: 5%;
+  font-weight: bold;
+  color: #000000;
+}
+
+.middle {
+  position: absolute;
+  top: 40%;
+  left: 45%;
+  margin: 5%;
+  transform: translate(-50%, -50%);
+  text-align: justify;
+  text-justify: inter-word;
+}
+
+hr {
+  margin: auto;
+  width: 60%;
+}
+</style>
+<body>
+
+  <div class="middle">
+    <div class="transbox">
+    <h1>Puzzle #1: <i>Skyline</i></h1>
+
+<g style="font-size:20px;">Fear the night, for what it hides. Hollow eyes and superficial minds…
+Can you see what the city is trying to tell you?</g>
+
+<img src="images/puzzle1_1.png" width="500" height="333">
+<img src="images/puzzle1_2.png" width="500" height="333">
+
+
+    </div>
+  </div>
+  
+
+
+
+
+</body>
+</html>
+
+
+
+<!--CONTENT GOES HERE-->
+
+<?php include("tail.php");?>  <!-- Contact inf and end body/html tags->
+
