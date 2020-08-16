4df5a108353aea747627098f064d1071426fc456
Christopher Brand <christopher.brand@berkeley.edu>
2020-08-15 23:21:51 -0700

    css formatting and stars

--- SVE Map Code/achievements.php
+++ SVE Map Code/achievements.php
@@ -10,21 +10,69 @@ list($achname, $achdescription, $achfluff, $achimpact, $achmaxnum, $achteam)=get
 
 <head>
   <style>
-  .collapsible {
-      background-color: hsla(0, 0%, 100%, 0.5);
-      text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;
+    #star-five {
+      margin: auto;
+      /*float: right;*/
+      position: relative;
+      display: block;
       color: yellow;
+      width: 0px;
+      height: 0px;
+      border-right: 20px solid transparent;
+      border-bottom: 14px solid yellow;
+      border-left: 20px solid transparent;
+      transform: rotate(35deg);
+    }
+    #star-five:before {
+      border-bottom: 16px solid yellow;
+      border-left: 6px solid transparent;
+      border-right: 6px solid transparent;
+      position: absolute;
+      height: 0;
+      width: 0;
+      top: -9px;
+      left: -13px;
+      display: block;
+      content: '';
+      transform: rotate(-35deg);
+    }
+    #star-five:after {
+      position: absolute;
+      display: block;
+      color: yellow;
+      top: 3px;
+      left: -21px;
+      width: 0px;
+      height: 0px;
+      border-right: 20px solid transparent;
+      border-bottom: 14px solid yellow;
+      border-left: 20px solid transparent;
+      transform: rotate(-70deg);
+      content: '';
+    }
+  .collapsible {
+      background-color: hsla(0, 100%, 0%, 0.8);
+      text-shadow: -1px 0 white, 0 1px white, 1px 0 white, 0 -1px white;
+      color: green;
       cursor: pointer;
       padding: 10px;
-      width: 100%;
       border: none;
       text-align: center;
       outline: none;
       font-size: 15px;
+      display: block;
+      width: 50%;
+      margin: 0 auto;
+      border-style: hidden hidden groove hidden;
+      font-size: 150%;
+      letter-spacing: 3px;
+      font-family:courier,arial,helvetica;
+      
     }
 
     .active, .collapsible:hover {
       background-color: #0000FF;
+      
     }
     
     .content {
@@ -33,9 +81,18 @@ list($achname, $achdescription, $achfluff, $achimpact, $achmaxnum, $achteam)=get
       overflow: hidden;
       transition: max-height 0.2s ease-out;
       /*background: rgba(255, 255, 255, 0.3) */
-      background-color: hsla(0, 0%, 100%, 0.3);
+      background-color: hsla(0, 100%, 0%);
       text-align: center;
       text-shadow: -1px 0 white, 0 1px white, 1px 0 white, 0 -1px white;
+      width: 50%;
+      margin-left: auto;
+      margin-right: auto;
+      color: blue;
+      font-size: 150%;
+      letter-spacing: 2px;
+      font-family:courier,arial,helvetica;
+      border-bottom-right-radius: 20px;
+      border-bottom-left-radius: 20px;
     }
       
   /*<!-- define general style elements -->*/
@@ -50,6 +107,7 @@ list($achname, $achdescription, $achfluff, $achimpact, $achmaxnum, $achteam)=get
         text-align: center;
         box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
         text-shadow: -1px 0 white, 0 1px white, 1px 0 white, 0 -1px white;
+        font-family:courier,arial,helvetica;
     }
     th.Sub{
         /*background-image: linear-gradient(to right, red , yellow);*/
@@ -86,11 +144,13 @@ foreach($achname as $aid => $aname) {
 	    echo "<tr class=\"Sub\">";
 	    foreach($achteam[$aid] as $atid => $ateam) {
 		  //  echo   "<th class=\"Sub\"> ",$ateam,"</th>";
-		    echo   "<th class=\"Sub\" background=\"/flags/flag",$atid,".png\"> ",$ateam,"</th>";
+		    echo   "<th class=\"Sub\" background=\"flags/flag",$atid,".png\"> ",$ateam,"</th>";
 		  //  echo " <image xlink:href=\"flags/flag",$tid,".png\" width=\"".$fw."\" height=\"".$fh."\"/>\n";
 		  //  echo   "<td class=\"Sub\"> Team number = ",$achflag[$aid][$atid],"</td>";
 	    }
+	    echo "<div id=\"star-five\"></div>";
 	    echo "</tr>";
+	    
 	    echo "</table>";
 	}
 }
@@ -99,7 +159,6 @@ foreach($achname as $aid => $aname) {
 ?>
 
 <script type="text/javascript">
-// pass PHP variable declared above to JavaScript variable
 // var achname = <?php echo json_encode($achname) ?>;
 
 var coll = document.getElementsByClassName("collapsible");
--- SVE Map Code/leaderboard.php
+++ SVE Map Code/leaderboard.php
@@ -10,26 +10,75 @@ list($blockname,$blockgeom,$blockown,$blockownid,$blocknext,$blocknextid,$teamid
 
 <head>
   <style>
-  p {
+    .star1{
+        float right;
+    }
+    .star2{
+        float left
+    }
+    #star-five {
+      margin: auto;
+      /*float: right;*/
+      position: relative;
+      display: block;
+      color: yellow;
+      width: 0px;
+      height: 0px;
+      border-right: 20px solid transparent;
+      border-bottom: 14px solid yellow;
+      border-left: 20px solid transparent;
+      transform: rotate(35deg);
+    }
+    #star-five:before {
+      border-bottom: 16px solid yellow;
+      border-left: 6px solid transparent;
+      border-right: 6px solid transparent;
+      position: absolute;
+      height: 0;
+      width: 0;
+      top: -9px;
+      left: -13px;
+      display: block;
+      content: '';
+      transform: rotate(-35deg);
+    }
+    #star-five:after {
+      position: absolute;
+      display: block;
+      color: yellow;
+      top: 3px;
+      left: -21px;
+      width: 0px;
+      height: 0px;
+      border-right: 20px solid transparent;
+      border-bottom: 14px solid yellow;
+      border-left: 20px solid transparent;
+      transform: rotate(-70deg);
+      content: '';
+    }
+    p {
       border: 2px solid white;
       color: white;
       text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;
       text-align: center;
       margin-bottom: 0;
+      border-radius: 25px;
+      font-size: 25px;
+      font-family:courier,arial,helvetica;
+      /*display:inline-block;*/
+      /*text-align: center;*/
+      width: 80%;
+      margin-left: auto;
+      margin-right: auto;
+    }
+    
+    .board {
+      background-color: hsla(0, 100%, 0%, 0.8);
       width: 50%;
       margin-left: auto;
       margin-right: auto;
-      border-radius: 25px;
-      font-size: 25px;
-  }
-    
-  .board {
-      background-color: hsla(0, 100%, 0%, 0.6);
-      width: 75%;
-      margin-left: auto;
-      margin-right: auto;
       margin-top: 25px;
-  }
+    }
   </style>
 </head>
 
@@ -38,8 +87,8 @@ list($blockname,$blockgeom,$blockown,$blockownid,$blocknext,$blocknextid,$teamid
 <?php 
 
 
-if(isset($blocknextid)) {
-    $lead = array_count_values($blocknextid);
+if(isset($blockownid)) {
+    $lead = array_count_values($blockownid);
     arsort($lead);
     // $test = implode(",", $lead);
     // echo "<p> ", $test, "</p>";
@@ -47,9 +96,17 @@ if(isset($blocknextid)) {
     echo "<div class=\"board\">";
     $iter = 1;
     foreach($lead as $tid => $tscore) {
-        echo "<p style= \"background: url(/flags/flag",$tid,".png)\" > ",$iter, ": ", $teamids[$tid]," - ", $tscore, "</p>";
-    
-        $iter++;
+        if($tid>0){
+            if($iter==1){
+                echo "<div id=\"star-five\" class=\"star1\"></div>";
+            }
+            echo "<p style= \"background: url(flags/flag",$tid,".png)\" > ",$iter, ": ", $teamids[$tid]," - ", $tscore, "</p>";
+            if($iter==1){
+                echo "<div id=\"star-five\" class=\"star2\"></div>";
+                echo "<br>";
+            }
+            $iter++;
+        }
     }
     echo "</div>";
  }
@@ -63,5 +120,6 @@ if(isset($blocknextid)) {
 
 
 </body>
+<!--CONTENT GOES HERE-->
 
 <?php include("tail.php");?>  <!-- Contact inf and end body/html tags->
