eb7b2caf83b86e35a14d99867a4951d45a39f850
Darren Bleuel <darren@nukees.com>
2020-08-18 11:07:38 -0800

    Files updated by Eric from 000webhost

--- SVE Map Code/menubar.php
+++ SVE Map Code/menubar.php
@@ -1,13 +1,13 @@
 	<ul class="customStyle">
 		<li class="customStyle"><a href="BRCMap_index.php">BRC Map</a></li>
-		<li class="customStyle"><a href="instructions.php">Game Board Rules</a></li>
+		<li class="customStyle"><a href="instructions.php">Game Rules</a></li>
         <li class="customStyle"><a href="abilities.php">Abilities</a></li>
 		<li class="customStyle"><a href="achievements.php">Achievements</a></li>
 		<li class="customStyle"><a href="teams.php">Team Portal</a></li>
 
-		<li style="float: right;"><a><?php if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {echo "<i>Action Points</i> (<i>Turns Remaining</i>): ", $action_points;} else {echo "<i>Action Points</i> (<i>Turns Remaining</i>): N/A";}?></a></li>		
-		<li style="float: right;"><a><?php if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {echo "<i>Impact Factor</i>: ", $impact;} else {echo "<i>Impact Factor</i>: N/A";}?></a></li>		
-		<li style="float: right;"><a><?php if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {echo "<i>Team</i>: ", $teamname;} else {echo "<i>Team</i>: N/A";}?></a></li>	
+		<li style="float: right;"><a><?php if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {echo "<i>Action Points</i> (<i>Turns Left</i>): ", $action_points;} else {echo "<i>Action Points</i> (<i>Turns Left</i>): N/A";}?></a></li>		
+		<li style="float: right;"><a><?php if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {echo "<i>Impact Factor</i>: ", $impact;} else {echo "<i>Impact Factor</i>: N/A";}?></a></li>	
+		<li style="float: right;"><a><?php if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {echo "<i>Team</i>: ", $teamname;} else {echo "<i>Team</i>: N/A";}?></a></li>
 	</ul>
 
 <!--		<li class="customStyle"><a href="register.php">Registration</a></li>
