7d2c484eab9357e70887233175363618d27b5818
Darren Bleuel <darren@nukees.com>
2020-08-23 13:59:18 -0800

    Added mail options without broadcasting my password to everyone on earth.  Maybe.

--- SVE Map Code/pq_submit.php
+++ SVE Map Code/pq_submit.php
@@ -1,24 +1,32 @@
 <?php include("db.php");?>  <!-- Login Session and database functions -->
 <?php
+// Setup mail sending library
+require_once('PHPMailer/PHPMailer.php');
+require_once('PHPMailer/SMTP.php');
+require_once('PHPMailer/Exception.php');
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
 $type = $_POST['type'];
 $answer = $_POST['answer'];
 $id = $_POST['id'];
 
-function advancepq($link, $type, $id, $teamid) {
+function advancepq($link, $type, $id, $teamid, $status=2) {
 	$typelet = substr($type,0,1);
-	$sql = "SELECT ".$typelet."id FROM ".$type."s WHERE ".$typelet."id > ".$id." LIMIT 1";
+	$sql = "SELECT ".$typelet."id FROM ".$type."s WHERE ".$typelet."id > ".$id." ORDER BY ".$typelet."id LIMIT 1";
 	if($result = mysqli_query($link, $sql))
-		if(mysqli_num_rows($result) == 1)
+		if(mysqli_num_rows($result) == 1) {
 			$next = mysqli_fetch_array($result)[$typelet."id"];
+			$nsql = "INSERT INTO ".$type."solved (".$typelet."id, teamid, status) VALUES (".$next.", ".$teamid.", 1);";
+			if (!mysqli_query($link, $nsql)) {
+				error("Error7 updating record: " . mysqli_error($link) . "\\n(".$sql.")", "teams.php");
+			}
+		}
 	mysqli_free_result($result);
-	$sql = "UPDATE ".$type."solved set status=2 WHERE teamid = ".$teamid." AND ".$typelet."id = ".$id.";";
+	$sql = "UPDATE ".$type."solved set status=".$status." WHERE teamid = ".$teamid." AND ".$typelet."id = ".$id.";";
 	if (!mysqli_query($link, $sql)) {
-		error("Error updating record: " . mysqli_error($link), "teams.php");
-	}
-	$sql = "INSERT INTO ".$type."solved (".$typelet."id, teamid, status) VALUES (".$next.", ".$teamid.", 1);";
-	if (!mysqli_query($link, $sql)) {
-		error("Error updating record: " . mysqli_error($link), "teams.php");
+		error("Error6 updating record: " . mysqli_error($link), "teams.php");
 	}
 }
 
@@ -27,7 +35,7 @@ if(isset($_FILES["answerfile"])) {
 	$uploadOk = 1;
 	$imageFileType = strtolower(pathinfo(basename($_FILES["answerfile"]["name"]),PATHINFO_EXTENSION));
 	$target_file = $target_dir."answerfile".$teamid."-".$type.$id.".".$imageFileType;
-	echo "Target file: ".$target_file."<br>";
+//	echo "Target file: ".$target_file."<br>";
 
 	// Check if image file is a actual image or fake image
 	if(isset($_POST["submit"])) {
@@ -59,12 +67,35 @@ if(isset($_FILES["answerfile"])) {
 	// Check if $uploadOk is set to 0 by an error
 	if (move_uploaded_file($_FILES["answerfile"]["tmp_name"], $target_file)) {
 //		echo "The file ". basename( $_FILES["answerfile"]["name"]). " has been uploaded.";
-		advancepq($link, $type, $id, $teamid);
+		advancepq($link, $type, $id, $teamid, 5);
 	   
 		$_SESSION['achievements'] = "You have successfully uploaded your proof of completing this ".$type.". You will receive an action point once a Senior Scientist has reviewed your proof and judged its scientific merit.  Until then, you can move on to the next ".$type."\\n";
 		if ($id==0) {
 			$_SESSION['achievements'] .= "As this was THE FIRST QUEST, after review, you'll be able to occupy your first block!\\n";
 			advancepq($link, "puzzle", 0, $teamid);
+
+			$mail = new PHPMailer();
+			$mail->isSMTP();
+			$mail->SMTPAuth = true;
+			$mail->SMTPSecure = 'ssl';
+			$mail->Host = 'smtp.gmail.com';
+			$mail->Port = '465'; //465
+			$mail->isHTML();
+			$mail->Username = 'nukees';
+			include("/home/nukees/sve_private/email.php");
+
+			//      $mail->SMTPSecure = "tls";
+			$mail->SetFrom('scientistsvseveryone@agnostica.com','Scientists Vs. Everyone');
+
+			$mail->Subject = "SvE-bot: New quest to review";
+			$mail->Body = "Beep.  This is Sciencebot.  There is a new image submitted for review for Quest#".$id." by team ".$teamname.".<br><br><a href=\"http://sve.nukees.com/".$rootdir."review_special_quests.php\">Click here to review it</a><br><br>Bloop.<br><br>--<br>Sciencebot<br>";
+			$mail->AddAddress("darren@nukees.com");
+			$mail->AddAddress("mahogony314@hotmail.com");
+			$mail->AddAddress("ericsweet2@gmail.com");
+
+
+			$mail->Send();
+
 		}
 
 	} else {
@@ -80,11 +111,11 @@ if(isset($_POST['answer'])) {
 		if(mysqli_num_rows($result) == 1)
 			$solution = mysqli_fetch_array($result)['solution'];
 	mysqli_free_result($result);
-	if($solution == strtolower($answer)) {
+	if(str_replace(" ","",strtolower($solution)) == str_replace(" ","",strtolower($answer))) {
 		advancepq($link, $type, $id, $teamid);
 		$sql = "UPDATE teams set action_points = action_points + 1  WHERE teamid = ".$teamid;
                 if (!mysqli_query($link, $sql)) {
-                        error("Error updating record: " . mysqli_error($link));
+                        error("Error5 updating record: " . mysqli_error($link));
                 }
 		$_SESSION['achievements'] = "Hooray!  You got the correct answer (".$solution.")!  Good job!  You've earned one action point!  Spend it on a new ability, or use it to activate one of your existing abilities.\\n";
 
@@ -93,4 +124,6 @@ if(isset($_POST['answer'])) {
 	}
 }
 
-header("Location: teams.php");
+header("Location: achievementcheck.php?l=teams.php");
+
+//header("Location: teams.php");
--- SVE Map Code/review_special_quests.php
+++ SVE Map Code/review_special_quests.php
@@ -3,40 +3,98 @@
 <?php include("menubar.php");?>  <!-- Common top menu bar -->
 
 <?php
-if(isset($_POST['Accept'])) {
-	$sql = "UPDATE teams set action_points = action_points + 1  WHERE teamid = ".$_POST['Team'];
-	if (!mysqli_query($link, $sql)) {
-		echo "Error updating record: " . mysqli_error($link);
-	} else {
-		echo "Accepted Quest ".$_POST['Accept']." by Team #".$_POST['Team'];
-		mysqli_free_result($result);
-		$sql = "UPDATE questsolved set status = 6 WHERE teamid = ".$_POST['Team']." AND qid = ".$_POST['Accept'];
+$sql = "SELECT qid, name FROM quests";
+
+if($result = mysqli_query($link, $sql)){
+	if(mysqli_num_rows($result) > 0){
+		while($row = mysqli_fetch_array($result)){
+			$qid = $row['abilityid'];
+			$qname[$qid] = $row['name'];
+		}
+	}
+	// Free result set
+	mysqli_free_result($result);
+}
+
+// Setup mail sending library
+require_once('PHPMailer/PHPMailer.php');
+require_once('PHPMailer/SMTP.php');
+require_once('PHPMailer/Exception.php');
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+if(isset($_POST['Team'])) {
+
+	$mail = new PHPMailer();
+	$mail->isSMTP();
+        $mail->SMTPAuth = true;
+        $mail->SMTPSecure = 'ssl';
+        $mail->Host = 'smtp.gmail.com';
+        $mail->Port = '465'; //465
+        $mail->isHTML();
+        $mail->Username = 'nukees';
+	include("/home/nukees/sve_private/email.php");
+
+//      $mail->SMTPSecure = "tls";
+        $mail->SetFrom('scientistsvseveryone@agnostica.com','Scientists Vs. Everyone');
+	$sql = "SELECT email FROM teams WHERE teamid = ".$_POST['Team'];
+        if($result = mysqli_query($link, $sql))
+                if(mysqli_num_rows($result) == 1)
+                        $email = mysqli_fetch_array($result)['email'];
+
+	if(isset($_POST['Accept'])) {
+		$sql = "UPDATE teams set action_points = action_points + 1  WHERE teamid = ".$_POST['Team'];
+		if (!mysqli_query($link, $sql)) {
+			echo "Error updating record: " . mysqli_error($link);
+		} else {
+			echo "Accepted Quest ".$_POST['Accept']." by Team #".$_POST['Team'];
+			mysqli_free_result($result);
+			$sql = "UPDATE questsolved set status = 6 WHERE teamid = ".$_POST['Team']." AND qid = ".$_POST['Accept'];
+
+			$mail->Subject = "Scientists vs Everyone: Quest submission accepted!";
+			$mail->Body = "Good news!<br><br>The image you uploaded for the \"".$qname[$_POST['Accept']]."\" quest was reviewed by our Scientists and deemed worthy of publication.  You have received an \"Action Point\" for completing this quest, which you can use to either gain or use an ability on the <a href=\"http://sve.nukees.com/".$rootdir."BRCMap_index.php\">Black Rock City Map</a>.  Thank you for playing Scientists vs. Everyone!<br><br>--<br>Blue and the Scientists!<br>";
+			$mail->AddAddress($email);
+			$mail->Send();
+			echo ", email sent";
+
+			if (!mysqli_query($link, $sql)) {
+				echo "Error updating record: " . mysqli_error($link);
+			} else {
+				echo ", and updated quest status<br>";
+			}
+			mysqli_free_result($result);
+		}
+
+		if(!rename("pq_answerfiles/".$_POST['File'],"pq_perm_answerfiles/".$_POST['File']))
+			echo " but couldn't delete file ".$_POST['File'];
+	}
+
+	if(isset($_POST["Reject"])) {
+		$stat = 7;
+		$mailtext = "Bad news!<br><br>The image you uploaded for the \"".$qname[$_POST['Reject']]."\" quest was reviewed by our Scientists but rejected for not meeting the conditions of the quest.  I'm sorry, but you will not get another chance to complete this quest.  Fortunately, there should be plenty more to pursue!";
+		if($_POST["Reject"] == 0) 
+			$mailtext .= "<br><br>However, since this was the very first quest, you can appeal this decision by replying to this email with an explanation and resubmitted photo.  But if you're just messing with us, we'll probably just delete your account.  There's too much fun and beauty in the world to waste it trolling, Amiright?";
+		$mailtext .= "Thank you for playing Scientists vs. Everyone!<br><br>--<br>Blue and the Scientists!<br>";
+		echo "Rejected Quest ".$_POST['Reject']." by Team #".$_POST['Team'];
+		$sql = "UPDATE questsolved set status = ".$stat." WHERE teamid = ".$_POST['Team']." AND qid = ".$_POST['Reject'];
+
+		$mail->Subject = "Scientists vs Everyone: Quest submission rejected!";
+		$mail->Body = $mailtext;
+		$mail->AddAddress($email);
+		$mail->Send();
+		echo ", email sent";
+
 		if (!mysqli_query($link, $sql)) {
 			echo "Error updating record: " . mysqli_error($link);
 		} else {
 			echo " and updated quest status";
 		}
 		mysqli_free_result($result);
+		if(!rename("pq_answerfiles/".$_POST['File'],"pq_perm_answerfiles/".$_POST['File']))
+			echo " but couldn't delete file ".$_POST['File'];
 	}
-
-	if(!rename("pq_answerfiles/".$_POST['File'],"pq_perm_answerfiles/".$_POST['File']))
-		echo " but couldn't delete file ".$_POST['File'];
 }
-
-if(isset($_POST["Reject"])) {
-	echo "Rejected Quest ".$_POST['Reject']." by Team #".$_POST['Team'];
-	$sql = "UPDATE questsolved set status = 7 WHERE teamid = ".$_POST['Team']." AND qid = ".$_POST['Reject'];
-	if (!mysqli_query($link, $sql)) {
-		echo "Error updating record: " . mysqli_error($link);
-	} else {
-		echo " and updated quest status";
-	}
-	mysqli_free_result($result);
-	if(!rename("pq_answerfiles/".$_POST['File'],"pq_perm_answerfiles/".$_POST['File']))
-		echo " but couldn't delete file ".$_POST['File'];
-}
-echo "<br>";
-
 ?>
 
 <style>
@@ -92,7 +150,7 @@ echo "<br>";
 }
 </style>
 
-
+<a href="review_special_quests.php" class="btn btn-warning">Refresh this page</a>
 <div class="row">
 	<div class="abilities" style="background-color:#FFBBBB">
 		<div class="rowheader">
@@ -111,6 +169,7 @@ foreach ($files as $file) {
 	$tid = substr($file,10,strpos($file,"-")-10);
 	$quest = substr($file,strpos($file,"-")+6,strpos($file,".")-strpos($file,"-")-6);
 	list($width, $height, $ftype, $attr) = getimagesize("pq_answerfiles/".$file);
+
 	while($height>500 && $width>500) {
 		$height=$height/2;
 		$width=$width/2;
